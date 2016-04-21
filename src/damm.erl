c7da09c26ae364f90750bb6f7ed853c4e01dc7cc
Yuki Ito <yuki@gnnk.net>
2016-04-22 00:06:40 +0900

    Implementation

--- src/damm.erl
+++ src/damm.erl
@@ -1,13 +1,34 @@
 -module(damm).
 
-%% API exports
--export([]).
+-export([checksum/1, encode/1, check/1]).
 
-%%====================================================================
-%% API functions
-%%====================================================================
+-define(TABLE, {{0, 3, 1, 7, 5, 9, 8, 6, 4, 2},
+                {7, 0, 9, 2, 1, 5, 4, 8, 6, 3},
+                {4, 2, 0, 6, 8, 7, 1, 3, 5, 9},
+                {1, 7, 5, 0, 9, 8, 3, 4, 2, 6},
+                {6, 1, 2, 3, 0, 4, 5, 9, 7, 8},
+                {3, 6, 7, 4, 2, 0, 9, 5, 8, 1},
+                {5, 8, 6, 9, 7, 2, 0, 1, 3, 4},
+                {8, 9, 4, 5, 3, 6, 2, 0, 1, 7},
+                {9, 4, 3, 8, 6, 1, 7, 2, 0, 5},
+                {2, 5, 8, 1, 4, 3, 6, 7, 9, 0}}).
 
+-define(LOOKUP(X, Y), (element(Y + 1, element(X + 1, ?TABLE)))).
 
-%%====================================================================
-%% Internal functions
-%%====================================================================
+-spec checksum(non_neg_integer()) -> 0..9.
+checksum(N) ->
+    S = integer_to_list(N),
+    checksum(S, 0).
+
+checksum([C|Rest], Acc) ->
+    checksum(Rest, ?LOOKUP(Acc, C - 48));
+checksum([], Acc) ->
+    Acc.
+
+-spec encode(non_neg_integer()) -> non_neg_integer().
+encode(N) ->
+    10 * N + checksum(N).
+
+-spec encode(non_neg_integer()) -> boolean().
+check(N) ->
+    checksum(N) == 0.
